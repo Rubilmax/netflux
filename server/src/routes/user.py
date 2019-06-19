22fc9fda83bf14816cb5797930d586eadf253c99
Romain Milon <rmilon@gmail.com>
2019-06-19 23:54:36 +0200

    Added login route

--- server/src/routes/user.py
+++ server/src/routes/user.py
@@ -4,10 +4,11 @@ Defines the blueprint for the users
 from flask import Blueprint
 from flask_restful import Api
 
-from resources import UserResource, UsersResource, UserCreateResource
+from resources import UserResource, UsersResource, UserCreateResource, UserLoginResource
 
 USER_BLUEPRINT = Blueprint("users", __name__)
 api = Api(USER_BLUEPRINT)
 api.add_resource(UserResource, "/user/<string:user_id>")
 api.add_resource(UsersResource, "/users")
 api.add_resource(UserCreateResource, "/user/add")
+api.add_resource(UserLoginResource, "/user/login")
