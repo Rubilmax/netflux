b213092fe852b98bd598d269fe11e2b9e06c8603
Maxime Shi <maxime.shi@student-cs.fr>
2019-06-19 15:52:21 +0200

    login page

--- client/src/login/Login.js
+++ client/src/login/Login.js
@@ -1,9 +1,20 @@
 import React from "react";
+import './Login.css';
 
 function Login() {
     return (
+        <div class="Login">
         <div>Connexion</div>
+        <div class="Connection">
+            <form action="/search" id="searchthis" method="get">
+                <label for="email"><b>Email</b></label>
+                <input type="text" placeholder="Adresse e-mail" />
+            </form>
+            <button type="submit" class="registerbtn">Se Connecter</button>
+        </div>
+        </div>
     );
 }
+
 
 export default Login;
