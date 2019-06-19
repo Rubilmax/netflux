0b2efe0423306648b7c5bfb8270fd81f43e6baf4
Maxime Shi <maxime.shi@student-cs.fr>
2019-06-19 14:55:29 +0200

    update disposition

--- client/src/home/Home.js
+++ client/src/home/Home.js
@@ -5,16 +5,18 @@ import './Home.css';
 function Home() {
     return (
         <div class="home">
+
+    
+            <div class="App-header">
+                <img src={logo} class="App-logo" alt="logo" />
+                <p>NETFLUX AND CHULL</p>
+            </div>
+
             <div class="recherche_p">
             <form action="/search" id="searchthis" method="get">
                 <input id="search" name="q" type="text" placeholder="Titre du film" />
                 <input id="search-btn" type="submit" value="Recherche &#x1F50D;" />
             </form>
-            </div>
-    
-            <div class="App-header">
-                <img src={logo} class="App-logo" alt="logo" />
-                <p>NETFLUX AND CHULL</p>
             </div>
         </div>
     );
