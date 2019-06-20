645e7dcc546e2d4f970975a4f7639e9448a89059
Maxime Shi <maxime.shi@student-cs.fr>
2019-06-20 11:02:36 +0200

    best movies home

--- client/src/home/Home.js
+++ client/src/home/Home.js
@@ -1,17 +1,44 @@
 import React from "react";
-
+import axios from "axios";
+import { Link } from "react-router-dom";
 import logo from '../logo.png';
 import './Home.css';
 
-function Home() {
-    return (
-        <div className="home-body">
-            <div className="home-logo">
-                <img src={logo} alt="logo" />
-                <p>NETFLUX & CHULL</p>
+
+class Home extends React.Component {
+
+    constructor() {
+        super();
+        this.state = { search: '', movies: [], shown: [] }
+        this.getMovies();
+    }
+
+    getMovies() {
+        return axios.get('http://localhost:5000/api/movies').then(response => this.setState({ movies: response.data.movies, shown: response.data.movies }));
+    }
+
+    render() {
+        return (
+            <div className="home-body">
+                <div className="home-logo">
+                    <img src={logo} alt="logo" />
+                    <p>NETFLUX & CHULL</p>
+                </div>
+                <div className="movies">
+                    { this.state.shown.map(movie =>
+                    <Link to={ `/movies/${ movie.movie_id }` } key={ movie.movie_id } >
+                        <div className="movie">
+                            <h3 className="movie-title">{ movie.title }</h3>
+                            <p className="movie-author">{ movie.author }</p>
+                            <i className="movie-year">{ movie.release_year }</i>
+                            <p className="movie-mark">{ movie.average_mark } &#9733;</p>
+                        </div>
+                    </Link>).slice(0,3)}
+
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Home;
