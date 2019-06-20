4e97ad2e182252e89bbd034865476bf3bc8d5ee2
Romain Milon <rmilon@gmail.com>
2019-06-20 18:28:30 +0200

    Displayed infos on account page

--- client/src/account/Account.js
+++ client/src/account/Account.js
@@ -1,14 +1,16 @@
 import React from "react";
 import axios from 'axios';
 import cookie from 'react-cookies';
+import { Link } from 'react-router-dom';
 
 import './Account.css';
+import loader from '../loader.gif';
 
 class Account extends React.Component {
 
     constructor() {
         super();
-        this.state = { message: '', user: {} };
+        this.state = { message: '', user: undefined };
 
         this.login = this.login.bind(this);
         if(cookie.load("user_id")) {
@@ -48,11 +50,26 @@ class Account extends React.Component {
                             </div>
                         }
                     </div> :
-                    <div className="member">
-                        <h1>Mon compte</h1>
-                        <p>Bienvenue sur votre page personnelle, { this.state.user.first_name } !</p>
-                        <h3>Les films que vous avez déjà vu</h3>
-                    </div>
+                    (this.state.user ?
+                        <div className="member">
+                            <h1>Mon compte</h1>
+                            <p>Bienvenue sur votre page personnelle, { this.state.user.first_name } !</p>
+                            <div className="container">
+                                <h2>Les films que vous avez déjà vu</h2>
+                                <div className="seen-movies">
+                                    { this.state.user.seen_movies.length ? this.state.user.seen_movies.map(movie =>
+                                        <Link to={ `/movies/${ movie.movie_id }` } key={ movie.movie_id } >
+                                            <div className="movie">
+                                                <h3 className="movie-title">{ movie.title }</h3>
+                                                <p className="movie-author">{ movie.author }</p>
+                                                <i className="movie-year">{ movie.release_year }</i>
+                                                <p className="movie-average">{ movie.average_mark } &#9733;</p>
+                                                <p className="movie-mark">Votre note : { movie.mark } &#9733;</p>
+                                            </div>
+                                        </Link>) : <h3>Aucun !</h3> }
+                                </div>
+                            </div>
+                        </div> : <img className="loader" src={ loader } alt="Loader" />)
                 }
             </div>
         );
